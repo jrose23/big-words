decff8665553be3fdea6194ca4d2c7817a139195
Justin <rosejustinb@gmail.com>
2022-03-25 15:50:08 -0400

    Removed react-fade-in package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import FadeIn from 'react-fade-in';
 import Header from './components/Header';
 import Word from './components/Word';
 import Definitions from './components/Definitions';
